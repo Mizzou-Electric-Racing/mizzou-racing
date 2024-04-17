cfb6cc59b91a4cea79db5b2fabb06d99a9fe2723
Aidan Puricelli <aidanpuricelli@gmail.com>
2024-04-16 23:36:19 -0500

    initial commit

---
+++ src/app/competition/ev-comp-overview/ev-comp-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ev-comp-overview',
+  templateUrl: './ev-comp-overview.component.html',
+  styleUrls: ['./ev-comp-overview.component.css']
+})
+export class EvCompOverviewComponent {
+
+}
